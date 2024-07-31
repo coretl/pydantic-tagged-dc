e5ab92c18bc8e16204f7cf5d267fe2eb84386067
Tom Cobb <tom.cobb@diamond.ac.uk>
2024-07-31 10:00:33 +0000

    Make it work

--- src/pydantic_tagged_dc/expressions.py
+++ src/pydantic_tagged_dc/expressions.py
@@ -2,21 +2,26 @@ from __future__ import annotations
 
 import dataclasses
 from abc import abstractmethod
+from functools import partial
 from typing import (
-    TYPE_CHECKING,
     Annotated,
     Any,
     Generic,
     Literal,
-    Protocol,
     TypeVar,
     Union,
     get_origin,
     get_type_hints,
-    runtime_checkable,
 )
 
-from pydantic import Discriminator, Field, RootModel, Tag, TypeAdapter
+from pydantic import (
+    Discriminator,
+    Field,
+    GetCoreSchemaHandler,
+    RootModel,
+    Tag,
+    TypeAdapter,
+)
 from pydantic.dataclasses import dataclass, rebuild_dataclass
 from pydantic.fields import FieldInfo
 
@@ -30,6 +35,8 @@ class _TaggedUnion:
         self.type_adapter = TypeAdapter(None)
 
     def add_member(self, cls: type):
+        if cls in self._members:
+            return
         self._members.add(cls)
         subclasses = tuple(Annotated[cls, Tag(cls.__name__)] for cls in self._members)
         if len(subclasses) > 1:
@@ -61,15 +68,10 @@ def _get_type_field(obj) -> str | None:
         return getattr(obj, "type", None)
 
 
-@runtime_checkable
-class _HasTaggedUnion(Protocol):
-    _tagged_union: _TaggedUnion
+_tagged_unions: dict[type, _TaggedUnion] = {}
 
 
-def _add_to_tagged_union(cls: _HasTaggedUnion) -> _HasTaggedUnion:
-    assert isinstance(
-        cls, _HasTaggedUnion
-    ), "The baseclass of {cls} does not define a '_tagged_union' attribute to add to"
+def __init_subclass__(cls: type):
     # Add a discriminator field to the class so it can
     # be identified when deserailizing, and make sure it is last in the list
     cls.__annotations__ = {
@@ -80,31 +82,34 @@ def _add_to_tagged_union(cls: _HasTaggedUnion) -> _HasTaggedUnion:
     # Replace any bare annotation with a discriminated union of subclasses
     # and register this class as one that refers to that union so it can be updated
     for k, v in get_type_hints(cls).items():
-        # This works for HasTaggedUnion[T] or HasTaggedUnion
-        origin = get_origin(v) or v
-        if isinstance(origin, _HasTaggedUnion):
-            origin._tagged_union.add_referrer(cls, k)
-    # Turn it into an actual dataclass
-    cls = dataclass(cls)
+        # This works for Expression[T] or Expression
+        tagged_union = _tagged_unions.get(get_origin(v) or v, None)
+        if tagged_union:
+            tagged_union.add_referrer(cls, k)
+
+
+def __get_pydantic_core_schema__(
+    cls, source_type: Any, handler: GetCoreSchemaHandler, tagged_union: _TaggedUnion
+):
     # Rebuild any dataclass (including this one) that references this union
     # Note that this has to be done after the creation of the dataclass so that
     # previously created classes can refer to this newly created class
-    cls._tagged_union.add_member(cls)
+    tagged_union.add_member(cls)
+    return handler(source_type)
+
+
+def discriminated_union_of_subclasses(cls):
+    tagged_union = _TaggedUnion()
+    _tagged_unions[cls] = tagged_union
+    cls.__init_subclass__ = classmethod(__init_subclass__)
+    cls.__get_pydantic_core_schema__ = classmethod(
+        partial(__get_pydantic_core_schema__, tagged_union=tagged_union)
+    )
     return cls
 
 
-if TYPE_CHECKING:
-    # From the type checker's point of view we are making a regular pydantic dataclass
-    add_to_tagged_union = dataclass
-else:
-    # But at runtime need to do some work to make the tagged union
-    add_to_tagged_union = _add_to_tagged_union
-
-
-@dataclass
-class Expression(_HasTaggedUnion, Generic[T]):
-    _tagged_union = _TaggedUnion()
-
+@discriminated_union_of_subclasses
+class Expression(Generic[T]):
     @abstractmethod
     def calculate(self) -> T:
         raise NotImplementedError(self)
@@ -113,11 +118,13 @@ class Expression(_HasTaggedUnion, Generic[T]):
         return RootModel(self).model_dump()
 
     @classmethod
-    def deserialize(cls, obj):
-        return cls._tagged_union.type_adapter.validate_python(obj)
+    def deserialize(cls, obj) -> Expression:
+        inst = _tagged_unions[Expression].type_adapter.validate_python(obj)
+        assert isinstance(inst, cls), "Expected {cls}, got {inst!r}"
+        return inst
 
 
-@add_to_tagged_union
+@dataclass
 class Value(Expression[T]):
     value: T = Field(description="Fixed value")
 
@@ -125,7 +132,7 @@ class Value(Expression[T]):
         return self.value
 
 
-@add_to_tagged_union
+@dataclass
 class Multiply(Expression[T]):
     left: Expression[T] = Field(description="Left hand value of the expression")
     right: Expression[T] = Field(description="Right hand value of the expression")
@@ -134,7 +141,7 @@ class Multiply(Expression[T]):
         return self.left.calculate() * self.right.calculate()
 
 
-@add_to_tagged_union
+@dataclass
 class Add(Expression[T]):
     left: Expression[T] = Field(description="Left hand value of the expression")
     right: Expression[T] = Field(description="Right hand value of the expression")
@@ -143,7 +150,7 @@ class Add(Expression[T]):
         return self.left.calculate() + self.right.calculate()
 
 
-@add_to_tagged_union
+@dataclass
 class Subtract(Expression[T]):
     left: Expression[T] = Field(description="Left hand value of the expression")
     right: Expression[T] = Field(description="Right hand value of the expression")
